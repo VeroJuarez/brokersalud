440419e6d5fba7d5fed80810f44e6178765ef834
VeroJuarez <veronicajuarez01@hotmail.com>
2025-09-26 15:23:19 -0300

    agrego opcion pregunta frecuentes

---
+++ components/PreguntasFrecuentes.jsx
@@ -0,0 +1,32 @@
+export default function PreguntasFrecuentes() {
+    return(
+        <section className='mt-[64px] lg:mt-[72px]'>
+            <div className='max-w-4xl mx-auto p-8'>
+                <h1 className='text-2xl font-bold mb-4 text-blue-600 text-center'>
+                    Preguntas Frecuentes
+                </h1>
+            </div>
+        </section>
+    )
+}
+
+// ¿Tenes dudas?
+//Tranquilo, nosotros te ayudamos a que el proceso de encontrar prepaga sea raápido, fácil y transparente
+
+//¿Por qué deberías contratar un plan de medicina Prepaga?
+//Los planes de medicina prepaga te permiten <b>acceder a los mejores centros médicos del país</b>
+// Además, te ofrecen atención médica de calidad, sin largas esperas y con una amplia gama de servicios médicos disponibles
+
+//¿Puedo cotizar una Prepaga si ya tengo obra social?
+//<b>¡Si!</b> Cualquier usuario de obra social puede contratar un servicio de medicina prepaga
+//Contactanos para ver qué opciones se ajustan a tus necesidades y presupuesto
+
+//¿Qué centros médicos del país trabajamos?
+//Tenemos en Capital Federela y Gran Buenos Aires
+//Algunos son el Hospital Italiano, Hospital Alemán, CEMIC, Hospital Británico,Fundación Favaloro,
+//FLENI, Hospital Austral, Sanatorio Finochieto, Clinica Bessone, Sanatorio Las Lomas, Sanatorio San Lucas,
+//entre otros
+
+//¿Tenes más dudas?
+//No te preocupes, podemos ayudarte en la decisión de qué medicina prepaga elegir.
+//Tan solo completá el formulario de contacto y te asesoramos sin compromiso.
